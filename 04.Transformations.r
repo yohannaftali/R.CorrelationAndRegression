5136b2dc930b12e1e11f6a8399e308f683e873c1
Yohan Naftali <yohan.naftali@gmail.com>
2017-05-25 22:17:47 +0700

    Create 04.Transformations.r

---
+++ 04.Transformations.r
@@ -0,0 +1,9 @@
+# Scatterplot with coord_trans()
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() + 
+  coord_trans(x = "log10", y = "log10")
+
+# Scatterplot with scale_x_log10() and scale_y_log10()
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() +
+  scale_x_log10() + scale_y_log10()
